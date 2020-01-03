d7711dd79ba009955620fb676772face81b65e9d
calexandredevWork <calexandredev@gmail.com>
2020-01-03 14:12:33 -0300

    refactory token

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import { search, searchAlbums, searchArtists, searchPlaylists, searchTracks } from './search'
 import { getAlbum, getAlbums, getAlbumTrack } from './album'
+import { API_URL } from './config';
 
-module.exports = {
-    search,
-    searchAlbums,
-    searchArtists,
-    searchPlaylists,
-    searchTracks,
-    getAlbum,
-    getAlbums,
-    getAlbumTrack
+export default class SpotifyWrapper {
+    constructor(options) {
+        this.apiURL = options.apiURL || API_URL,
+        this.token = options.token 
+    }
 }
+
+// module.exports = {
+//     search,
+//     searchAlbums,
+//     searchArtists,
+//     searchPlaylists,
+//     searchTracks,
+//     getAlbum,
+//     getAlbums,
+//     getAlbumTrack
+// }
