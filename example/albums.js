32b523f548d900934789a4b88b08cd237d77d028
calexandredev <calexandredev@gmail.com>
2020-01-04 23:30:59 -0300

    refactory search

--- example/albums.js
+++ example/albums.js
@@ -1,7 +1,11 @@
 global.fetch = require('node-fetch')
 
-import { searchAlbums } from '../src/search.js'
+import SpotifyWrapper from '../src/index.js'
 
-const albums = searchAlbums('Incubus')
+const spotify = new SpotifyWrapper({
+    token: 'BQDqSiaV1IbKR1n0jp0hePxTjdXLQ63dm7AEGYEUSpBQ7g2PW8a0u5oGBwQCjwk1Q6cVYyPg8iAyY21x5xIsEYu85pjACqz_cczIJCRqi2SypREza3V0a3byY507slBfrmbsPno1-69Rryifx_gjQFZQGa1AQJZgVL1JHQ'
+})
+
+const albums = spotify.search.albums('Incubus')
 
 albums.then(data => data.albums.items.map(item => console.log(item.name)))
