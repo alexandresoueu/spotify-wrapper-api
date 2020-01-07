87f76b336cf8a9e258ea867afb61df3c3ca4df95
calexandredev <calexandredev@gmail.com>
2020-01-07 07:45:33 -0300

    example change token

--- src/config.js
+++ src/config.js
@@ -1,9 +1,3 @@
-const TOKEN_API = 'BQATsnczqc-EfPF-L7eF7bOit7P-C39hPNhZ2QLY6Sh9OPghQokFxaDUvGnhoWtN6BhXXDY9oC61Kt2xLrAqH4Fd31uJMM74G05oZZj18j9zS7DBf5cBnBGLpveAdaG-a2SS07YsTJcwo-IzOHFnaWLhPTdD-LWQqWuaGw'
+const API_URL = 'https://api.spotify.com/v1';
 
-export const API_URL = 'https://api.spotify.com/v1'
-
-export const HEADERS = {
-    headers: {
-        Authorization: `'Bearer ${TOKEN_API}'`
-    }
-}
+export default API_URL;
