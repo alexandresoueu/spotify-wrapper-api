8219f8982ef1ec3ae2c3244d0fd5429f7dbc6024
calexandredev <calexandredev@gmail.com>
2020-01-07 07:46:16 -0300

    new index

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import search from './search'
-import album from './album'
-import { API_URL } from './config';
-
+import API_URL from './config';
+import toJSON from './utils';
+import album from './album';
+import search from './search';
 
 export default class SpotifyWrapper {
-    constructor(options) {
-        this.apiURL = options.apiURL || API_URL,
-        this.token = options.token,
-        this.album = album.bind(this)(),
-        this.search = search.bind(this)()
-    }
+  constructor(options) {
+    this.apiURL = options.apiURL || API_URL;
+    this.token = options.token;
+    this.album = album.bind(this)();
+    this.search = search.bind(this)();
+  }
 
-    request(url) {
-        const headers = {
-            headers: {
-                Authorization: `'Bearer ${this.token}'`
-            }
-        }
-
-        return fetch(url, headers)
-    }
+  request(url) {
+    const headers = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.token}`,
+      },
+      mode: 'cors',
+    };
+    return fetch(url, headers).then(toJSON);
+  }
 }
